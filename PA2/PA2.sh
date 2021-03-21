813c37b563785401c0dc02adce55221866e1a80d
zoo868e <zoo868e@gmail.com>
2021-03-21 18:28:26 +0800

    PA2
    7 direct done

--- PA2/PA2.sh
+++ PA2/PA2.sh
@@ -2,13 +2,19 @@ ls ~/X &> /dev/null || ln -s /dev/null ~/X
 ls ~/_ &> /dev/null || ln -s /dev/null ~/_
 cd ~/PA2;rm -rf rooms; tar -xf rooms.tar
 cd rooms/receivingRoom;
-PS1=">"
 
 alias x='cat'
 alias score='echo You have scored `(cd ~/PA2/rooms/treasureRoom;ls [^9d.]* 2>/dev/null | wc -l)`0 points.'
 alias quit='score;exit'
 alias nogo='echo You cannot go that way.'
-
+alias s='basename `pwd -P` | grep n && cd .. || cd s &> ~/X && disp || nogo'
+alias n='basename `pwd -P` | grep s && cd .. || cd n &> ~/X && disp || nogo'
+alias e='basename `pwd -P` | grep w && cd .. || cd e &> ~/X && disp || nogo'
+alias w='basename `pwd -P` | grep e && cd .. || cd w &> ~/X && disp || nogo'
+alias sw='basename `pwd -P` | grep ne && cd .. || cd sw &> ~/X && disp || nogo'
+alias nw='basename `pwd -P` | grep se && cd .. || cd nw &> ~/X && disp || nogo'
+alias d='basename `pwd -P` | grep u && cd .. || cd d &> ~/X && disp || nogo'
+alias u='__0__ && basename `pwd -P` | grep d && cd .. || cd u &> ~/X && disp || nogo'
 alias l='mv 1 9&>~/X;disp'
 alias disp='__4__&&(head -n `ls [91]` description;(mv 9 1;dispmessages))'
 alias dispmessages='fgrep -hw `find . -maxdepth 1 -type f | cut -c3- | paste -d "" ~/PA2/rooms/es -` -h ~/PA2/rooms/There | cut -d \; -f 1;'
