b54e6d4fd1500faa4d268f551200c6179c88831c
zoo868e <zoo868e@gmail.com>
2021-04-14 13:12:15 +0800

    Fix some bug
    Add the direction u and d to condition which forgot to translate.
    Pass the basic test 1 and test 2

--- PA3/PA3/checkYourOutput.sh
+++ PA3/PA3/checkYourOutput.sh
@@ -2,4 +2,4 @@ echo "Be patient. This might take up to 2 minutes to run..."
 (source ~/PA3/testInput.sh)>~/PA3/testOutput.txt
 echo Finished Running. Now checking for differences to the expected output:
 cd ~/PA3
-diff -y testOutput.txt expectedOutput.txt && echo You passed the basic test.
+diff testOutput.txt expectedOutput.txt && echo You passed the basic test.
